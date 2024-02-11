ddab25c8a501e41e4dabe92bae61c9c30cec7061
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-02-11 23:11:34 +0530

    added leftsidebar and top panel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,9 @@ import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
-    <div className="h-screen">
-      <UserButton afterSignOutUrl="/"/>
-    </div>
+    <>
+      {/* <UserButton afterSignOutUrl="/"/> */}
+      <h1 className="head-text text-left">Home</h1>
+    </>
   )
 }
---
+++ components/shared/Rightsidebar.tsx
@@ -0,0 +1,5 @@
+function Rightsidebar() {
+    return <h1>Rightsidebar</h1>
+}
+
+export default Rightsidebar;
