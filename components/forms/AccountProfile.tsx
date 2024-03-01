01afcb4350edecc2cf9eb081956050d72b9229e5
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-03-01 22:27:03 +0530

    onboarding completed

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 'use client' // for using forms
-import React from 'react'
+import React, { useState, type ChangeEvent } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { UserValidation } from '@/lib/validations/user'
@@ -19,6 +19,7 @@ import {
 
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import { Textarea } from '../ui/textarea'
 
 interface Props {
   user: {
@@ -33,15 +34,39 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+  console.log(user);
+  const [files, setFiles] = useState<File[]>([])
+
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo: '',
-      name: '',
-      username: '',
-      bio: ''
+      profile_photo:  '',  //Insert photo from user.image
+      name: user?.name || '',
+      username: user?.username || '',
+      bio: user?.bio || ''
     }
   })
+
+  const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
+    e.preventDefault()
+    const fileReader = new FileReader();
+
+    if ((e.target.files != null) && e.target.files.length > 0) {
+      const file = e.target.files[0]
+
+      setFiles(Array.from(e.target.files))
+
+      if (!file.type.includes('image')) return
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || ''
+
+        fieldChange(imageDataUrl)
+      }
+
+      fileReader.readAsDataURL(file)
+    }
+  }
 
   function onSubmit (values: z.infer<typeof UserValidation>) {
     console.log(values)
@@ -80,17 +105,76 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     />
                       )}
                   </FormLabel>
-                 <FormControl>
-                   <Input placeholder="your username" {...field} />
+                 <FormControl className="flex-1 text-base-semibold text-gray-200">
+                   <Input
+                      type="file"
+                      accept="image/*"
+                      placeholder="Upload a photo"
+                      className="account-form_image-input"
+                      onChange={(e) => { handleImage(e, field.onChange) }}
+                    />
                  </FormControl>
-                 <FormDescription>
-                   This is your public display name.
-                 </FormDescription>
-                 <FormMessage />
                </FormItem>
              )}
            />
-          <Button type="submit">Submit</Button>
+
+            <FormField
+             control={form.control}
+             name="name"
+             render={({ field }) => (
+               <FormItem className="flex flex-col w-full gap-3">
+                 <FormLabel className='text-base-semibold text-light-2'>
+                  Name
+                  </FormLabel>
+                 <FormControl>
+                   <Input
+                      type="text"
+                      className="account-form_image"
+                      {...field}
+                    />
+                 </FormControl>
+               </FormItem>
+             )}
+           />
+
+            <FormField
+             control={form.control}
+             name="username"
+             render={({ field }) => (
+              <FormItem className="flex flex-col w-full gap-3">
+              <FormLabel className='text-base-semibold text-light-2'>
+               Username
+               </FormLabel>
+              <FormControl>
+                <Input
+                      type="text"
+                      className="account-form_image"
+                      {...field}
+                    />
+                 </FormControl>
+               </FormItem>
+             )}
+           />
+
+            <FormField
+             control={form.control}
+             name="bio"
+             render={({ field }) => (
+              <FormItem className="flex flex-col w-full gap-3">
+                 <FormLabel className='text-base-semibold text-light-2'>
+                  Bio
+                  </FormLabel>
+                 <FormControl>
+                   <Textarea
+                      rows={10}
+                      className="account-form_image"
+                      {...field}
+                    />
+                 </FormControl>
+               </FormItem>
+             )}
+           />
+          <Button type="submit" className='bg-primary-500'>Submit</ Button>
          </form>
        </Form>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      serverActions: {},
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "img.clerk.com",
+        },
+        {
+          protocol: "https",
+          hostname: "images.clerk.dev",
+        },
+        {
+          protocol: "https",
+          hostname: "uploadthing.com",
+        },
+        {
+          protocol: "https",
+          hostname: "placehold.co",
+        },
+      ],
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+  
+  export default nextConfig;
