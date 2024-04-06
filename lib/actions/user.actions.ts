27833c2b439b6118e84e05c624ec475de9fe4e7c
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-04-06 23:25:31 +0530

    search implemented

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache'
 import User from '../models/user.model'
 import { connectToDB } from '../moongose'
 import Thread from '../models/thread.model'
+import { FilterQuery, SortOrder } from 'mongoose'
 
 interface Params {
   userId: string
@@ -93,3 +94,53 @@ export async function fetchUserPosts(userId: string){
     throw new Error(`Failed to fetch user posts : ${error.message}`)
   }
 }
+
+
+export async function fetchSearchUsers({
+  userId, 
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc"
+} : {
+  userId: string, 
+  searchString? : string,
+  pageNumber?: number, 
+  pageSize?: number, 
+  sortBy?: SortOrder
+}) {
+  try{
+    connectToDB()
+
+    //no of users to skip base on paege no & size
+    const skipAmount = (pageNumber-1) * pageSize;
+    const regex = new RegExp(searchString, 'i')
+
+    const query : FilterQuery<typeof User>= {
+      id: {$ne: userId}
+    }
+
+    if(searchString.trim() !== ''){
+      query.$or = [
+        { username: { $regex: regex }},
+        {name: {$regex: regex}}
+      ]
+    }
+
+    const sortOptions = { createdAt: sortBy }
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize)
+
+    const totalUsersCount = await User.countDocuments(query)
+
+    const users = await usersQuery.exec();
+    const isNext = totalUsersCount > skipAmount + users.length
+
+    return {users, isNext}
+  }
+  catch (error: any){
+    throw new Error(`Failed to fetch users for search : ${error.message}`)
+  }
+}  
